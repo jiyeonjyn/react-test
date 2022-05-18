6f3f8886ecae007e0f62c518e8c2abfcdfcc9e9b
jiyeonjyn <ljy7183@naver.com>
2022-05-18 11:55:04 +0900

    Feat: add unit tests and component tests

---
+++ src/components/form/tests/habitAddForm.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import HabitAddForm from '../habitAddForm';
+import renderer from 'react-test-renderer';
+
+describe('HabitAddForm', () => {
+  // 스냅샷 테스트
+  it('renders', () => {
+    const component = renderer.create(<HabitAddForm onAdd={jest.fn()} />);
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  // 작동 테스트
+  describe('Form Submit', () => {
+    let onAdd;
+    let input;
+    let button;
+    beforeEach(() => {
+      onAdd = jest.fn();
+      render(<HabitAddForm onAdd={onAdd} />);
+      input = screen.getByPlaceholderText('Habit');
+      button = screen.getByText('Add');
+    });
+
+    it('calls onAdd when button is clicked and valid habit is entered', () => {
+      userEvent.type(input, 'New Habit');
+      userEvent.click(button);
+
+      expect(onAdd).toHaveBeenCalledWith('New Habit');
+    });
+
+    it('does not call onAdd when the habit is empty', () => {
+      userEvent.type(input, '');
+      userEvent.click(button);
+
+      expect(onAdd).toHaveBeenCalledTimes(0);
+    });
+  });
+});
